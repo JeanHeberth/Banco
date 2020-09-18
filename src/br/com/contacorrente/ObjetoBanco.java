309393e3fa20b28e52c8fd5cb89c66b5ba31b45b
jean.vieira <jean.vieira@EST-CALL12.hepta.com.br>
2020-09-18 10:56:29 -0300

    Método deposito criado

---
+++ src/br/com/contacorrente/ObjetoBanco.java
@@ -0,0 +1,35 @@
+package br.com.contacorrente;
+
+import javax.swing.JOptionPane;
+
+public class ObjetoBanco {
+
+	public static void main(String[] args) {
+
+		Banco objeto = new Banco();
+		int op = 0;
+
+		do {
+			op = (Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a opcao desejada: \n"
+					+ "1 - Cadastrar a Conta\n" + "2 - Deposito\n" + "3 - Saque\n" + "4 - Saldo\n" + "5 - Sair")));
+
+			// usar o switch case
+			switch (op) {
+			case 1:
+				objeto.entraCadastro();
+				break;
+
+			case 2:
+				objeto.entraDeposito();
+
+				break;
+			}
+
+			// obj.entr
+			// obj.gfogfork
+
+		} while (op != 5);
+
+	}
+
+}
